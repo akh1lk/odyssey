7b3971c45723738d25c1b09844cfbe53aebff939
Gabriel Castillo <gac232@cornell.edu>
2025-03-27 20:01:47 -0400

    Create PropEval tests. - Test all the different operations - Test complex propositions

--- test/test_odyssey.ml
+++ test/test_odyssey.ml
@@ -1,37 +1,60 @@
 open OUnit2
 open Odyssey
 
-(* Helper function to test parsing and evaluation *)
-let test_parse_and_eval name expr data expected_eval _ =
-  Printf.printf "Testing: %s\n" name;
-  Printf.printf "Expression: %s\n" expr;
-  let parsed = PropEval.parse_prop expr in
-  Printf.printf "Parsed: %s\n" (PropEval.print_prop parsed);
-  let eval_result = PropEval.eval_prop parsed (PropEval.create_data data) in
-  Printf.printf "Evaluation Result: %b\n" eval_result;
-  assert_equal expected_eval eval_result
+(* Helper function to create tests for PropEval.eval_prop *)
+let make_test name prop_str data_lst expected =
+  name >:: fun _ ->
+  let prop = PropEval.parse_prop prop_str in
+  let data = PropEval.create_data data_lst in
+  let result = PropEval.eval_prop prop data in
+  assert_equal expected result ~printer:string_of_bool
 
-(* Simplified test cases *)
 let suite =
-  "Simplified Proposition Tests"
+  "PropEval Tests"
   >::: [
-         "Test x as true"
-         >:: test_parse_and_eval "x as true" "x" [ "x true" ] true;
-         "Test ~x as false"
-         >:: test_parse_and_eval "~x as false" "~x" [ "x true" ] false;
-         "Test (x ^ z) as true"
-         >:: test_parse_and_eval "(x ^ z) as true" "(x ^ z)"
-               [ "x true"; "z true" ] true;
-         "Test (x v y) as true"
-         >:: test_parse_and_eval "(x v y) as true" "(x v y)"
-               [ "x true"; "y false" ] true;
-         "Test (x => z) as true"
-         >:: test_parse_and_eval "(x => z) as true" "(x => z)"
-               [ "x true"; "z true" ] true;
-         "Test (x ^ (~y)) as false"
-         >:: test_parse_and_eval "(x ^ (~y)) as false" "(x ^ (~y))"
-               [ "x true"; "y true" ] false;
+         (* Variables *)
+         make_test "Test var true" "x" [ "x true" ] true;
+         make_test "Test var false" "x" [ "x false" ] false;
+         (* NOT ( ~ )*)
+         make_test "Test not true" "~x" [ "x false" ] true;
+         make_test "Test not false" "~x" [ "x true" ] false;
+         make_test "Test double not" "~(~x)" [ "x true" ] true;
+         (* AND ( ^ ) *)
+         make_test "Test and true" "x ^ y" [ "x true"; "y true" ] true;
+         make_test "Test and false 1" "x ^ y" [ "x false"; "y true" ] false;
+         make_test "Test and false 2" "x ^ y" [ "x true"; "y false" ] false;
+         (* OR ( v ) *)
+         make_test "Test or true 1" "x v y" [ "x true"; "y false" ] true;
+         make_test "Test or true 2" "x v y" [ "x false"; "y true" ] true;
+         make_test "Test or false" "x v y" [ "x false"; "y false" ] false;
+         (* IMPLIES ( -> ) *)
+         make_test "Test implies true 1" "x -> y" [ "x true"; "y true" ] true;
+         make_test "Test implies true 2" "x -> y" [ "x false"; "y false" ] true;
+         make_test "Test implies false" "x -> y" [ "x true"; "y false" ] false;
+         (* BICONDITIONAL ( <-> ) *)
+         make_test "Test biconditional true 1" "x <-> y" [ "x true"; "y true" ]
+           true;
+         make_test "Test biconditional true 2" "x <-> y"
+           [ "x false"; "y false" ] true;
+         make_test "Test biconditional false 1" "x <-> y"
+           [ "x true"; "y false" ] false;
+         make_test "Test biconditional false 2" "x <-> y"
+           [ "x false"; "y true" ] false;
+         (* Complex Prop Tests *)
+         make_test "Test complex expression 1" "(x ^ y) -> z"
+           [ "x true"; "y true"; "z true" ]
+           true;
+         make_test "Test complex expression 2" "(x ^ y) -> z"
+           [ "x true"; "y true"; "z false" ]
+           false;
+         make_test "Test complex expression 3" "(x ^ y) -> z"
+           [ "x false"; "y true"; "z false" ]
+           true;
+         make_test "Test multiple operators" "x -> (y -> z)"
+           [ "x true"; "y true"; "z true" ]
+           true;
+         make_test "Test not and" "~(x ^ y)" [ "x true"; "y true" ] false;
+         make_test "Test not or" "~(x v y)" [ "x false"; "y false" ] true;
        ]
 
-(* Run tests *)
-let () = run_test_tt_main suite
+let _ = run_test_tt_main suite
